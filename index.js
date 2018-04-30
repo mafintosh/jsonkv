c53d800860596205b9a8edf79d7df558857ef6ab
Mathias Buus <mathiasbuus@gmail.com>
2018-04-30 16:12:28 +0200

    improve iterator perf

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const raf = require('random-access-file')
 const nanoiterator = require('nanoiterator')
 const toStream = require('nanoiterator/to-stream')
 
+const ITERATOR_BUFFER = 256
+
 jsonkv.createWriteStream = require('./write-stream')
 module.exports = jsonkv
 
@@ -45,6 +47,7 @@ class DB {
         if (!header) return cb(new Error('Database has an invalid header'))
 
         self.valueSize = header.valueSize
+        self.pageSize = 4 + self.valueSize + 1 + 2
         self.length = header.length
         self.offset = idx + 2 + 1
         self.opened = true
@@ -64,16 +67,15 @@ class DB {
     const self = this
     const g = opts.gte || opts.gt
     const l = opts.lte || opts.lt
-    const start = g && typeof g === 'object' ? g : {key: g}
-    const end = l && typeof l === 'object' ? l : {key: l}
+    const start = g && (typeof g === 'object' ? g : {key: g})
+    const end = l && (typeof l === 'object' ? l : {key: l})
 
     var offset = 0
     var limit = 0
+    var block = Buffer.alloc(0)
+    var ptr = 0
 
-    return nanoiterator({
-      open,
-      next
-    })
+    return nanoiterator({open, next})
 
     function open (cb) {
       if (!start) return self.open(cb)
@@ -82,16 +84,16 @@ class DB {
       function onstart (err, val, seq) {
         if (err) return cb(err)
 
-        offset = self.offset + seq * (4 + self.valueSize + 1 + 2)
+        offset = self.offset + seq * self.pageSize
         limit = val ? self.length - seq : 0
+
         if (limit <= 0) return cb(null)
 
         const cmp = self.sort(val, start)
         if (opts.gte && cmp >= 0) return cb(null)
         if (opts.gt && cmp > 0) return cb(null)
 
-        offset += (4 + self.valueSize + 1 + 2)
-        next((err, val) => onstart(err, val, seq + 1))
+        getValue(self, seq + 1, (err, val) => onstart(err, val, seq + 1))
       }
     }
 
@@ -102,14 +104,20 @@ class DB {
       }
 
       if (!limit) return cb(null, null)
+      if (ptr < block.length) return onblock(null, block)
 
-      self.storage.read(offset, 4 + self.valueSize, function (err, buf) {
+      ptr = 0
+      const blockSize = Math.min(limit, ITERATOR_BUFFER) * self.pageSize
+      self.storage.read(offset, blockSize, onblock)
+
+      function onblock (err, buf) {
         if (err) return cb(err)
 
-        offset += 4 + self.valueSize + 1 + 2
+        block = buf
+        offset += self.pageSize
         limit--
 
-        const val = decodeValue(buf)
+        const val = decodeValue(buf.toString('utf-8', ptr, ptr += self.pageSize))
         if (!val) return cb(new Error('Invalid database entry'))
 
         if (end) {
@@ -119,7 +127,7 @@ class DB {
         }
 
         cb(null, val)
-      })
+      }
     }
   }
 
@@ -153,21 +161,20 @@ class DB {
 }
 
 function getValue (db, idx, cb) {
-  const size = 4 + db.valueSize + 1 + 2
-  const offset = db.offset + idx * size
+  const offset = db.offset + idx * db.pageSize
 
-  db.storage.read(offset, size, function (err, buf) {
+  db.storage.read(offset, db.pageSize, function (err, buf) {
     if (err) return cb(err)
-    const val = decodeValue(buf)
+    const val = decodeValue(buf.toString())
     if (!val) return cb(new Error('Invalid database entry'))
     cb(null, val)
   })
 }
 
-function decodeValue (buf) {
-  const val = decode('[' + buf + 'null]')
+function decodeValue (str) {
+  const val = decode('[' + str + 'null]')
   if (val) return val[0]
-  return decode(buf.toString())
+  return decode(str)
 }
 
 function openAndGet (db, key, opts, cb) {
